6727965fdee740de3ec81e1416e6495ecc1be7b4
Bogdan M <bogdancons99@gmail.com>
2021-01-10 00:39:00 +0200

    connect auth epics

--- lib/src/epics/app_epics.dart
+++ lib/src/epics/app_epics.dart
@@ -1,16 +1,20 @@
 import 'package:instagram_clone/src/actions/index.dart';
+import 'package:instagram_clone/src/data/auth_api.dart';
+import 'package:instagram_clone/src/epics/auth_epics.dart';
 import 'package:instagram_clone/src/models/index.dart';
 import 'package:meta/meta.dart';
 import 'package:redux_epics/redux_epics.dart';
 
 class AppEpics{
 
-  const AppEpics({@required String api}): assert(api != null), _api= api;
+  const AppEpics({@required AuthApi api}): assert(api != null), _authApi= api;
 
-  final String _api;
+
+  final AuthApi _authApi;
 
   Epic<AppState> get epics {
     return combineEpics<AppState>(<Epic<AppState>>[
+      AuthEpics(api: _authApi).epics,
     ]);
   }
   
--- lib/src/init/init.dart
+++ lib/src/init/init.dart
@@ -1,3 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:instagram_clone/src/data/auth_api.dart';
 import 'package:instagram_clone/src/epics/app_epics.dart';
 import 'package:instagram_clone/src/models/index.dart';
 import 'package:instagram_clone/src/reducer/reducer.dart';
@@ -5,7 +9,10 @@ import 'package:redux/redux.dart';
 import 'package:redux_epics/redux_epics.dart';
 
 Future<Store<AppState>> init() async {
-  final AppEpics epic = AppEpics();
+
+  await Firebase.initializeApp();
+  final AuthApi _authApi = AuthApi(auth: FirebaseAuth.instance, firestore: FirebaseFirestore.instance);
+  final AppEpics epic = AppEpics(api: _authApi);
 
   return Store<AppState>(reducer, initialState: AppState.initialState(), middleware: <Middleware<AppState>>[
     EpicMiddleware<AppState>(epic.epics),
